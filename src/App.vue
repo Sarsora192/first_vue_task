1d530f1d7519e9d0db209f268259353f487581f7
Sarsora192 <105431742+Sarsora192@users.noreply.github.com>
2022-06-11 16:42:02 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<PageTitle :title="'Sara\'s Task'" />
+		<Dashboard />
+	</div>
+</template>
+
+<script>
+	import PageTitle from '@/components/Page/PageTitle.vue';
+	import Dashboard from '@/components/Dashboard/Dashboard.vue';
+
+	export default {
+		components: {
+			PageTitle,
+			Dashboard
+		}
+	};
+</script>
+
+<style lang="scss">
+	#app {
+		width: 100%;
+	}
+
+	[v-cloak] {
+		display: none;
+	}
+</style>
---
+++ src/components/Dashboard/DashboardInfo.vue
@@ -0,0 +1,104 @@
+<template>
+	<div class="dashboard-info">
+		<ul class="dashboard-info__list">
+			<li class="dashboard-info__item">
+				<UITag
+					tag-name="Tasks"
+					:tag-value="totalTaskList"
+				/>
+			</li>
+			<li class="dashboard-info__item">
+				<UITag
+					tag-name="Tasks Done"
+					:tag-value="totalTaskListDone"
+				/>
+			</li>
+		</ul>
+		<ul class="dashboard-info__list">
+			<li
+				v-if="totalTaskListDone !== 0"
+				class="dashboard-info__item"
+			>
+				<UIButton
+					class="button--bg-color-error button--small"
+					@button-click="removeTaskListDone()"
+				>
+					<span class="button__icon">
+						<i class="icon">
+							<FontAwesomeIcon icon="trash" />
+						</i>
+					</span>
+					<span class="button__text">
+						tasks done
+					</span>
+				</UIButton>
+			</li>
+			<li class="dashboard-info__item">
+				<UIButton
+					class="button--bg-color-error button--small"
+					@button-click="removeTaskList()"
+				>
+					<span class="button__icon">
+						<i class="icon">
+							<FontAwesomeIcon icon="trash" />
+						</i>
+					</span>
+					<span class="button__text">
+						Delete All
+					</span>
+				</UIButton>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	import {mapGetters, mapActions} from 'vuex';
+	import UIButton from '@/components/UI/UIButton.vue';
+	import UITag from '@/components/UI/UITag.vue';
+
+
+	export default {
+		name: 'DashboardInfo',
+		components: {
+			UIButton,
+			UITag
+		},
+		computed: {
+			...mapGetters({
+				totalTaskList: 'getTotalTaskList',
+				totalTaskListDone: 'getTotalTaskListDone'
+			})
+		},
+		methods: {
+			...mapActions([
+				'removeTaskList',
+				'removeTaskListDone'
+			])
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+	.dashboard-info {
+		width: 100%;
+		padding: 0.5rem;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: flex-end;
+		font-size: 1.4rem;
+		background-color: $color-ghost;
+
+		&__list {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: flex-end;
+			align-items: center;
+			list-style: none;
+		}
+
+		&__item {
+			margin: 0.5rem;
+		}
+	}
+</style>
